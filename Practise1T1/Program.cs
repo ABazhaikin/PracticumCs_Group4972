7abe50054d7c8306529437844d69875b03afcdd3
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-10 17:20:56 +0300

    1 task added

---
+++ Practise1T1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №1
+// Напишите программу, которая на вход принимает число и
+// Выдает его квадрат
+
+Console.WriteLine("Input number");
+//Cчитываем данные с консоли
+string? inputNum = Console.ReadLine();//??"0";
+//Проверяем, чтобы данные были не пустыми   
+if (inputNum != null)
+{
+    // Парсим введенное число
+    // int num = int.Parse(inputNum);
+    // Находим квадрат числа
+    // int res = num*num;
+    // Выводим данные в консоль
+    // Console.WriteLine(res); 
+
+}
+
+Console.WriteLine("Num sqr" + (int)Math.Pow(int.Parse(inputNum), 2));
