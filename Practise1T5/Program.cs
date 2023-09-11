7af337073488a3ec39ef90091e098d369f1ce896
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-11 16:25:36 +0300

    Task 5 added

---
+++ Practise1T5/Program.cs
@@ -0,0 +1,31 @@
+﻿// int numN = int.Parse(Console.ReadLine()??"0");
+// int evNumN = (-1) * numN;
+
+// while (evNumN < numN)
+// {
+//     Console.Write(evNumN + ",");
+//     evNumN = evNumN + 1; 
+// }
+// Console.WriteLine(numN);
+
+// Второй способ решения задачи с перечислением
+
+// int numN = int.Parse(Console.ReadLine()??"0");
+
+// for (int i = -numN; i < numN; i++)
+// {
+//     Console.Write (i + ", ");
+// }
+//     Console.Write (numN);
+
+// Третий способ решения задачи с перечислением
+
+// int NumN = int.Parse(Console.ReadLine()??"0");
+// int evNum = (-1) * NumN;
+
+// while (evNum < NumN)
+// {
+//     Console.Write (evNum + ", ");
+//     evNum++;
+// }
+//     Console.Write(NumN);
