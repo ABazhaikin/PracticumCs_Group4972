8d74b9eb2cc8795954fdb39cb316fa62c60608c7
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-18 16:26:09 +0300

    Task11 added

---
+++ Practise2T11/Program.cs
@@ -0,0 +1,33 @@
+﻿void Variant1()
+{
+
+    int randomNumb = new System.Random().Next(100, 1000);
+    System.Console.WriteLine(randomNumb);
+    char[] digitChar = randomNumb.ToString().ToCharArray();
+    System.Console.Write(digitChar[0]);
+    System.Console.Write(digitChar[2]);
+
+}
+
+void Variant2()
+{
+
+    Console.WriteLine("-1");
+    Console.WriteLine("Enter numb");
+    string stringNum = Console.ReadLine() ?? "0";
+    int num = Convert.ToInt32(stringNum);
+
+    if (num == -1) return;
+    if (stringNum.Length != 3)
+        Console.WriteLine("not3");
+    else
+    {
+        Console.WriteLine((num / 100) * 10 + (num % 10));
+    }
+    // Console.WriteLine(stringNum.Length != 3 ? "not 3" : (num / 100) * 10 + (num % 10));
+    Console.WriteLine();
+}
+
+
+// Variant1();
+Variant2();
