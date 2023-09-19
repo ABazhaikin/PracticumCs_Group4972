ed3f8e65b8253ffd6222a27e56cb49484d835bb5
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-19 20:09:29 +0300

    HW #10 updated

--- Practise2T10/Program.cs
+++ Practise2T10/Program.cs
@@ -10,7 +10,6 @@
 
 void Variant2()
 {
-
     Console.WriteLine("Enter num:");
     string stringNum = Console.ReadLine() ?? "0";
     int num = Convert.ToInt32(stringNum);
@@ -19,11 +18,27 @@ void Variant2()
         Console.WriteLine("not 3 dig");
     else
     {
-        Console.WriteLine((num / 10) % 10 );
+        Console.WriteLine((num / 10) % 10);
     }
-    // Console.WriteLine(stringNum.Length != 3 ? "not 3" : (num / 100) * 10 + (num % 10));
+}
+
+void Variant3()
+{
+    Console.WriteLine("Enter num:");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num >= 100 && num < 1000)
+    {
+        int secDig = (num / 10) % 10;
+        Console.WriteLine("vtoroe chislo: " + secDig);
+    }
+    else
+    {
+        Console.WriteLine("not 3 dig");
+    }
 }
 
 
 //Variant1();
-Variant2();
+//Variant2();
+Variant3();
