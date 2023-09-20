72221c1cc2cc0fb9ac2c72d6abf9f55fa58c3abd
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-20 12:29:35 +0300

    HW13 updated

--- Practise2T13/Program.cs
+++ Practise2T13/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Variant1()
+{
+    Console.WriteLine("Enter num:");
+    int num = int.Parse(Console.ReadLine() ?? "0");
+
+    if (num >= 100)
+    {
+        int thirdDig = (num / 100) % 10;
+        Console.WriteLine("3d dig is: " + thirdDig);
+    }
+    else
+    {
+        Console.WriteLine("Less than 3 dig in ur num");
+    }
+}
+Variant1();
