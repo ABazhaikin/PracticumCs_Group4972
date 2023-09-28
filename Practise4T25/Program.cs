bf3aae60bc84fe10595935c4e074ccd1751dbdc0
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-28 18:55:36 +0300

    HW25 added

---
+++ Practise4T25/Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+long SimpleSum(int numA, int numB)
+{
+    long res = 1;
+    while (numB > 0)
+    {
+        res = res * numA;
+        numB= numB-1;
+    }
+    return res;
+}
+
+void PrintResult(string msg, long result)
+{
+    Console.WriteLine(msg + result);
+}
+
+
+int numberA = ReadData("Enter A: ");
+int numberB = ReadData("Enter B: ");
+long result = SimpleSum(numberA, numberB);
+PrintResult("Result: ", result);
