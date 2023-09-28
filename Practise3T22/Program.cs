9c42358ae97cc386a23fb78887e5638b2394a62e
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-28 15:00:13 +0300

    HW 3 added

--- Practise3T22/Program.cs
+++ Practise3T22/Program.cs
@@ -1,27 +1,27 @@
-﻿// int ReadData(string msg)
-// {
-//     Console.WriteLine(msg);
-//     int num = int.Parse(Console.ReadLine() ?? "0");
-//     return num;
-// }
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
 
-// string BuildLine(int x, int pow)
-// {
-//     string res = string.Empty;
-//     for (int i = 1; i <= x; i++)
-//     {
-//         res = res + Math.Pow(i, pow) + " ";
-//     }
-//     return res;
-// }
+string BuildLine(int x, int pow)
+{
+    string res = string.Empty;
+    for (int i = 1; i <= x; i++)
+    {
+        res = res + Math.Pow(i, pow) + " ";
+    }
+    return res;
+}
 
-// void PrintResult(string msg)
-// {
-//     Console.WriteLine(msg);
-// }
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
 
-// int x = ReadData("Enter num: ");
+int x = ReadData("Enter num: ");
 
-// PrintResult(BuildLine(x, 1));
-// PrintResult(BuildLine(x, 2));
+PrintResult(BuildLine(x, 1));
+PrintResult(BuildLine(x, 3));
 
