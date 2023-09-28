c4b99f256e9c914109194c103ac7e8f8ac1c394a
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-28 19:03:20 +0300

    HW27 added

---
+++ Practise4T27/Program.cs
@@ -0,0 +1,24 @@
+﻿int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine() ?? ",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Считает длинну числа
+int digiSum(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        int digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+    return sum;
+}
+
+int number = Prompt("Enter num: ");
+Console.WriteLine("sum of "+number+" dig is: "+ digiSum(number));
