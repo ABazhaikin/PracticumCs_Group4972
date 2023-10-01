421cdc5608d318c1c1b09fa032ed7229e927eade
aleksei <aleksei.bazhaikin@gmail.com>
2023-10-01 17:00:09 +0300

    HW5 done

---
+++ Practise5T36/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int[] genArray(int length, int lowBroder, int maxBorder)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(lowBroder, maxBorder + 1);
+    }
+    return arr;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+int sumNegPos(int[] array)
+{
+    int sum = 0
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+void printResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int[] array = genArray(10, 0, 99);
+printArray(array);
+int negativeSumm = sumNegPos(array);
+printResult($"{negativeSumm} sum of elements");
---
+++ Practise5T38/Program.cs
@@ -0,0 +1,53 @@
+﻿
+double[] genArray(int length, double lowBorder, double maxBorder)
+{
+    double[] arr = new double[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = random.NextDouble() * (maxBorder - lowBorder) + lowBorder;
+    }
+    return arr;
+}
+
+void printArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+double difference(double[] array)
+{
+    if (array.Length == 0)
+    {
+        return 0;
+    }
+    double min = array[0];
+    double max = array[0];
+    foreach (double num in array)
+    {
+        if (num < min)
+        {
+            min = num;
+        }
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    return max - min;
+}
+
+void printResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+double[] array = genArray(10, 0.0, 99.0);
+printArray(array);
+double differenceSum = difference(array);
+printResult($"Difference between min and max is {differenceSum}");
