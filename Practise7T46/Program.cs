ae08d4eda8cef84ea2652cc346acdb1c3ad0089d
aleksei <aleksei.bazhaikin@gmail.com>
2023-10-06 21:09:04 +0300

    Day 7 solved

---
+++ Practise7T46/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив размером MxN, заполненный случайными целыми числами.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+int[,] Fill2DArray(int countRow, int countColumn, int maxBorder, int lowBorder)
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rnd.Next(lowBorder, maxBorder);
+
+        }
+    }
+
+    return array2D;
+}
+
+void print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] colors = new ConsoleColor[] { ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan,    ConsoleColor.DarkBlue, ConsoleColor.DarkCyan, ConsoleColor.DarkGray, ConsoleColor.DarkGreen, ConsoleColor.DarkMagenta, ConsoleColor.DarkRed, ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.Green, ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.White, ConsoleColor.Yellow};
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = colors[new System.Random().Next(0,16)];
+            Console.Write(matrix[i, j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Enter rows: ");
+int columns = ReadData("Enter columns: ");
+
+int[,] arr2D = Fill2DArray(row, columns, 100, 10);
+
+print2DArray(arr2D);
+print2DArrayColor(arr2D);
---
+++ Practise7T48/Program.cs
@@ -0,0 +1,85 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+int[,] Fill2SumInd(int countRow, int countColumn)
+{
+    System.Random rnd = new System.Random();
+    int[,] array = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array[i, j] = i + j;
+
+        }
+    }
+
+    return array;
+}
+
+void print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            Console.Write(matrix[i, j] + "\t ");
+        }
+        Console.WriteLine(matrix[i, matrix.GetLength(1) - 1] + "]");
+    }
+}
+
+int row = ReadData("Enter rows: ");
+int columns = ReadData("Enter columns: ");
+
+int[,] arr2D = Fill2SumInd(row, columns);
+
+print2DArray(arr2D);
+
+// Вывод сообщения и запись введённых данных
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     string value = Console.ReadLine() ?? ",";
+//     int number = Convert.ToInt32(value);
+
+//     return number;
+// }
+// // Заполняет массив
+// void FillingArray(int[,] array, int m, int n)
+// {
+//     for (int i = 0; i < m; i++)
+//         for (int j = 0; j < n; j++)
+//             array[i, j] = i + j;
+// }
+// // Выводит элементы массива в консоль
+// void OutputArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("{");
+
+//         for (int j = 0; j < array.GetLength(1) - 1; j++)
+//             Console.Write(array[i, j] + ",\t");
+
+//         Console.WriteLine(array[i, array.GetLength(1) - 1] + "}");
+//     }
+// }
+
+// int m = Prompt("Введите m: ");
+// int n = Prompt("Введите n: ");
+
+// int[,] array = new int[m, n];
+// FillingArray(array, m, n);
+// OutputArray(array);
